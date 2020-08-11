1a3178e28a746f58e2352dc9e27d16cca39a1586
aurahair <69343202+aurahair@users.noreply.github.com>
2020-08-11 21:04:40 +0900

    g

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {Button, Progress} from 'semantic-ui-react'
 function App() {
   return (
-  <p>효선 </p>
-  });>
+  <div>
+  <p>안녕하세요! </p>
+  <Button Primary> hi </Button>
+    <Progress percent = {33}/>
+    </div>
   );
 }
 
